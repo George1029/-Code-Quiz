2e85847d018e56dfade31ce2d8aa086190b3b7df
George Ellison <georgeellison1029@gmail.com>
2021-09-09 21:46:18 -0500

    added new questions & new java variables

---
+++ assets/js.js
@@ -0,0 +1,9 @@
+var timeLeft = 75
+var timerID;
+var timerElement = document.getElementById("timer")
+var beginButton = document.getElementById("begin")
+var nextButton = document.getElementById("");
+var containerQuestionElement = document.getElementById("");
+// add start container var startContainerElement = document.getElementById("start-container");
+var questionElement = document.getElementById("");
+var answerButtonsElement = document.getElementById("");
--- assets/questions.js
+++ assets/questions.js
@@ -1,18 +1,44 @@
-var questionsArray = [
+var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        question: "What is the correct way to write a Javascript array?",
+        answers: [
+            { text: "var musicGenre = (0:'pop', 1:'hip hop', 2:'rock')", correct: false },
+            { text: "var musicGenre = ['pop', 'hip hop', 'rock']", correct: true },
+            { text: "var musicGenre = (pop, hip hop, rock)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+
+    {
+        question: "Commonly used data types DO NOT include",
+        answers: [
+            { text: "var secondQues = strings", correct: false },
+            { text: "var secondQues = booleans", correct: false },
+            { text: "var secondQues = numbers", correct: false },
+            { text: "var secondQues = alerts", correct: true }
+
+        ]
+
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        question: "What does the term DRY mean in software development?",
+        answers: [
+            { text: "var thirdQues = Defense Rushing Yards", correct: false },
+            { text: "var thirdQues = Design Reference Year", correct: false },
+            { text: "var thirdQues = Dont Repeat Yourself", correct: true },
+            { text: "var thirdQues = Doing Recovery Yourself", correct: false }
+        ]
     },
+
     {
-        title: "Arrays in Javascript can be used to store",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-      },
-  ];
-  
+        question: "Which is NOT a programming language?",
+        answers: [
+            {text: "var fourthQues = HTML", correct:true},
+            {text: "var fourthQues = JavaScript", correct:false},
+            {text: "var fourthQues = Python", correct:false},
+            {text: "var fourthQues = Ruby",correct:false}
+
+        ]
+    },
+
+]
