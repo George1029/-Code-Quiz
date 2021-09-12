a0225c0a154dfff96bb6d2f7fafe8e8e8fd909d8
George Ellison <georgeellison1029@gmail.com>
2021-09-12 14:28:55 -0500

    check answer function

--- assets/js.js
+++ assets/js.js
@@ -10,7 +10,7 @@ var containerQuestionElement = document.getElementById("quest-container");
 var startContainerElement = document.getElementById("start-container");
 var questionElement = document.getElementById("quest");
 var answerButtonsElement = document.getElementById("answer-btns");
-var checkAnswerEl = document.getElementById("check-answer");
+var checkAnswerElement = document.getElementById("check-answer");
 var viewHighScores = document.getElementById("look-at-scores");
 var submitButton = document.getElementById("submit");
 var clearScoreButton = document.getElementById("clear");
@@ -48,4 +48,85 @@ function startGame() {
     // Timer will start as soon as start button is clicked
     timeCountdown();
     NextQuestion();
+};
+
+// click to next question
+function NextQuestion() {
+    resetStateOfQuestion();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+// show questions
+function showQuestion(question) {
+    question.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+};
+
+//reset
+function resetStateOfQuestion() {
+
+    nextButton.classList.add ("hide")
+    checkAnswerElement.classList.add("hide")
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+
+};
+
+//function to select answer
+function selectAnswer (event) {
+var selectedAnswer = event.target;
+//check if answer is correct
+var correct = selectedAnswer.dataset.correct;
+checkAnswerElement.classList.remove("hide")
+if (correct) {
+    checkAnswerElement.innerHTML = "right!";
+} else {
+    checkAnswerElement.innerHTML = "Sorry that was wrong.";
+    if (timeLeft <= 10) {
+        timeLeft = 0;
+    } else {
+        // If the aswer is wrong, deduct time by 10
+        timeLeft -= 10;
+    }
+}
+
+Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+})
+
+if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+    checkAnswerElement.classList.remove("hide")
+} else {
+    beginButton.classList.remove("hide")
+    saveScore();
+}
+};
+
+// Check and show the correct answer by set the buttons colors
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+};
+
+
+// Remove all the classes
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
 };
